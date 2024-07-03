c8841b8465bf4b68c4bc75a991ca7aed7ea2471f
baedori99 <baejason36@gmail.com>
2024-07-03 10:14:46 +0900

    24-07-03 making MBTI chatbot ing

--- Study_Streamlit/streamlit_llm_chatbot.py
+++ Study_Streamlit/streamlit_llm_chatbot.py
@@ -1,9 +1,42 @@
 import streamlit as st
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain_openai import ChatOpenAI
+template = """\
+당신은 상대방의 MBTI를 듣고 그 MBTI에 관한 정보를 알려주는 로봇입니다.
+MBTI에 관한 질문에만 답변해주세요.
+500자 이내로 상대방의 CHAT에 존댓말로 답변해주세요
+"""
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", template),
+        ("human", "{input}")
+    ]
+)
+model = ChatOpenAI(model_name="gpt-3.5-turbo")
+chain = prompt | model | StrOutputParser()
+
+container = st.empty()
+handler = CustomHandler(container)
+answer = self.chain.invoke(
+    {"input": question},
+    {"callbacks": [handler]}
+)
+session_key = "chat_history"
 
 # 빈 리스트 만들기
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
+if session_key not in st.session_state:
+    st.session_state[session_key] = []
 
+if "chain" in st.session_state:
+    chain = st.sessioin_state["chain"]
+else:
+    
 # 첫 채팅을 시작할 때 첫 인사 출력
 if len(st.session_state["messages"]) == 0:
     greeting = "안녕하세요. 챗봇입니다."
@@ -24,6 +57,7 @@ if question:
     st.session_state["messages"].append({"role":"user", "content":question})
     
     # 답변 출력
-    answer = "즐건 저녁되세요!"
+    answer = model.invoke(question).content
     st.chat_message("assistant").markdown(answer)
     st.session_state["messages"].append({"role":"assistant","content":answer})
+
